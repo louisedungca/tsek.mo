f62e9f5f9ac9a84737116131a385f06db6812ee4
Louise Dungca <dungca.louisechristine@gmail.com>
2024-02-25 13:58:00 +0800

    chore: modify back button path and remove due_date validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,11 +2,4 @@ class Task < ApplicationRecord
   belongs_to :category
 
   validates :task_item, presence: true
-  before_validation :set_default_due_date
-
-  private
-
-  def set_default_due_date
-    self.due_date = Time.zone.now if due_date.nil?
-  end
 end
