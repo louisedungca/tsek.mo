8a06ea6adf12c627535d34b7a29e2423ba378de0
Louise Dungca <dungca.louisechristine@gmail.com>
2024-02-25 16:16:39 +0800

    feat: add edit task action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
-  before_action :set_task
+  before_action :set_category
+  before_action :set_task, except: [:create]
 
   def create
     @task = @category.tasks.build(task_params)
@@ -13,6 +14,19 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @category.tasks.update(task_params)
+      flash[:notice] = "Task item successfully updated!"
+      redirect_to category_path(@category)
+    else
+      flash[:alert] = "Oops, there was a problem updating the task item. Please try again."
+      render :edit, status: 422
+    end
+  end
+
   def destroy
     @task.destroy
     flash[:alert] = "Task permanently deleted."
@@ -21,14 +35,25 @@ class TasksController < ApplicationController
 
   private
 
-  def set_task
-    @category = Category.find(params[:category_id])
-    @task = Task.find(params[:id])
-  end
-
   def task_params
     params
       .require(:task)
       .permit(:task_item, :due_date, :is_completed)
+  end
+
+  def set_category
+    @category = Category.find(params[:category_id])
+
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "Oh no! The category you were looking for does not exist."
+    redirect_to categories_path
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "Oh no! The task you were looking for does not exist."
+    redirect_to category_path(@category)
   end
 end
