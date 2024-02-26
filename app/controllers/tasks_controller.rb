06ca919874d133fcf1335fe0c23098400eb4d0ef
Louise Dungca <dungca.louisechristine@gmail.com>
2024-02-26 21:02:15 +0800

    fix: change redirect path on update and delete task actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,7 +20,8 @@ class TasksController < ApplicationController
   def update
     if @task.update(task_params)
       flash[:notice] = "Task item successfully updated!"
-      redirect_to category_path(@category)
+      redirect_back(fallback_location: root_path)
+      # redirect_to category_path(@category)
     else
       flash[:alert] = "Oops, there was a problem updating the task item. Please try again."
       render :edit, status: 422
@@ -32,7 +33,8 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     flash[:alert] = "Task permanently deleted."
-    redirect_to category_path(@category)
+    redirect_back(fallback_location: root_path)
+    # redirect_to category_path(@category)
   end
 
   private
