53aa9c064b9ec2e6530befecbc1a58ea1ff628ea
Louise Dungca <dungca.louisechristine@gmail.com>
2024-03-01 15:16:32 +0800

    feat: add sorted scope on task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,6 +5,8 @@ class Task < ApplicationRecord
 
   scope :due_today, ->(user) { joins(:category).where("due_date <= ? AND categories.user_id = ?", Date.current, user.id) }
 
+  scope :sorted, -> { joins(:category).where.not(id: nil).order(arel_table[:due_date].asc.nulls_last).order(:updated_at) }
+
   def task_overdue?
     !is_completed? && due_date? && due_date < Date.current
   end
