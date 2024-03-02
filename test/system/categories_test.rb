3fa77a2e5cf1264a699173cfaad6608574736d30
Louise Dungca <dungca.louisechristine@gmail.com>
2024-03-02 22:24:28 +0800

    test: create test for destroy category action

--- test/system/categories_test.rb
+++ test/system/categories_test.rb
@@ -17,8 +17,7 @@ class CategoriesTest < ApplicationSystemTestCase
     assert_text "Category successfully added!"
     visit category_path(@category)
   end
-
-  test "should update Category" do
+  test "should update Category on Category page" do
     visit category_path(@category)
     assert_selector ".category-name", text: @category.category_name.upcase
     click_on @category.category_name
@@ -30,6 +29,20 @@ class CategoriesTest < ApplicationSystemTestCase
     assert_text "Category name successfully updated!"
     assert_selector ".category-name", text: new_category_name.upcase
   end
+  test "should destroy Category on Category page" do
+    visit category_path(@category)
+
+    within(".delete-btn") do
+      delete_button = find("a[href='#{category_path(@category)}'][data-turbo-method='delete']")
+
+      accept_confirm do
+        delete_button.click
+      end
+    end
+
+    assert_text "Category permanently deleted."
+  end
+
 
   private
 
@@ -46,35 +59,3 @@ class CategoriesTest < ApplicationSystemTestCase
     assert page.has_css?(".navbar"), "Navbar not found."
   end
 end
-
-=begin
-  test "should update Category" do
-    visit category_url(@category)
-    click_on "Edit this category", match: :first
-
-    fill_in "Category name", with: @category.category_name
-    fill_in "User", with: @category.user_id
-    click_on "Update Category"
-
-    assert_text "Category was successfully updated"
-  end
-
-  test "should destroy Category" do
-    visit categories_url
-    find("#delete-1")
-
-    accept_alert do
-      click_button "OK"
-    end
-
-    assert_text "Category permanently deleted."
-  end
-
-  test "should log out current user" do
-    login
-
-    click_on "Logout"
-    page.driver.browser.switch_to.alert.accept
-    visit new_user_session_path
-  end
-=end
