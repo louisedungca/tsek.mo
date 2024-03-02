cb33ad5e9be6ea3dcbb956b29856751e9fe60af4
Louise Dungca <dungca.louisechristine@gmail.com>
2024-03-02 22:03:33 +0800

    test: add more fixtures and create edit category test method

--- test/system/categories_test.rb
+++ test/system/categories_test.rb
@@ -1,24 +1,34 @@
 require "application_system_test_case"
 class CategoriesTest < ApplicationSystemTestCase
-  include Devise::Test::IntegrationHelpers
-
   setup do
-    `rake RAILS_ENV=test db:seed`
-    @user = users(:user_one)
-    @category = categories(:one)
+    @user = users(:user_two)
+    @category = categories(:categ_two)
 
     login
   end
   test "visiting the index" do
     assert_selector "h1", text: "NEW CATEGORY"
   end
-  test "should create category and redirect to the category page" do
+  test "should create new Category and redirect to the Category page" do
     click_on "New Category"
     fill_in "Category Name:", with: @category.category_name
     click_on "Submit"
 
     assert_text "Category successfully added!"
     visit category_path(@category)
+  end
+
+  test "should update Category" do
+    visit category_path(@category)
+    assert_selector ".category-name", text: @category.category_name.upcase
+    click_on @category.category_name
+
+    new_category_name = "New Category Name"
+    fill_in "Category Name:", with: new_category_name
+    click_on "Submit"
+
+    assert_text "Category name successfully updated!"
+    assert_selector ".category-name", text: new_category_name.upcase
   end
 
   private
@@ -33,9 +43,7 @@ class CategoriesTest < ApplicationSystemTestCase
   end
 
   def assert_navbar
-    assert_selector "img[alt='tsek.mo_logo']"
-    assert_selector "div", text: "hello, #{@user.email}"
-    assert_selector "a", text: "LOGOUT"
+    assert page.has_css?(".navbar"), "Navbar not found."
   end
 end
 
@@ -49,7 +57,6 @@ end
     click_on "Update Category"
 
     assert_text "Category was successfully updated"
-    click_on "Back"
   end
 
   test "should destroy Category" do
