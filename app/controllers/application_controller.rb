3155b0968116f7ad03877d370e233ed21ec514dd
Louise Dungca <dungca.louisechristine@gmail.com>
2024-03-12 21:43:42 +0800

    refactor: use category_name class method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,7 @@ class ApplicationController < ActionController::Base
   private
 
   def record_not_found_error
-    controller_name = controller_path.singularize
-
-    flash[:alert] = "Hmm. The #{controller_name} that you are looking for does not exist."
+    flash[:alert] = "Hmm. The #{controller_name.singularize} that you are looking for does not exist."
     redirect_to root_path
   end
 end
